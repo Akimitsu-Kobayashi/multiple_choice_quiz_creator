2dd712bb7d78e514e94fed24a4f047a289c33108
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-04-08 00:09:50 +0800

    made the formating of getting the inputs better

--- create_simple_quiz.py
+++ create_simple_quiz.py
@@ -10,11 +10,11 @@ user chose to exit.
 while True:
     #ask for inputs (question, possible answers, and correct answer)
     question = input("Enter a question: ")
-    option_a = input("Enter option a")
-    option_b = input("Enter option b")
-    option_c = input("Enter option c")
-    option_d = input("Enter option d")
-    correct_answer = input("Enter Correct answer")
+    option_a = input("Enter option a: ")
+    option_b = input("Enter option b: ")
+    option_c = input("Enter option c: ")
+    option_d = input("Enter option d: ")
+    correct_answer = input("Enter Correct answer: ")
 
     #write the collected data to a .txt file
     with open("question_and_answer.txt","a") as quiz_file:
